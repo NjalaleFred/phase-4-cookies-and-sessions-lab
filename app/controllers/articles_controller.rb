11d8acea1fb45dc9a9a5681648fb504633201e29
Njalale <fnjalale@gmail.com>
2023-07-02 02:00:24 +0300

    Completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,16 @@ class ArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
-    render json: article
+    cookies[:page_views] ||= 0
+    cookies[:page_views] = cookies[:page_views].to_i + 1
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    if cookies[:page_views] > 3
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    else
+      render json: article
+    end
+
   end
 
   private
